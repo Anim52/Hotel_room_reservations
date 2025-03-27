2c03487be53cbb2084cccf606a34e5bd21fc2f2e
Anim52 <dimasplai662@gmail.com>
2025-03-27 19:48:57 +0300

    Добавлены некоторые стили а также проведена работа с окнами авторизации, регистрации и основным окном

--- AplicationApp/Views/Windows/MainWindow.xaml.cs
+++ AplicationApp/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using AplicationApp.Views.Pages;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,70 @@ namespace AplicationApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void LoadCurrentUserId()
+        {
+           // string currentUserLogin = App.CurrentUserLogin;
+
+           // Получаем пользователя из базы данных по логину
+           //var user = _context.User.FirstOrDefault(u => u.Login == currentUserLogin);
+
+           // if (user != null)
+           // {
+           //     _currentUserId = user.Id;
+           // }
+           // else
+           // {
+           //     MessageBox.Show("Пользователь не найден.");
+           // }
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            //string currentUserLogin = App.CurrentUserLogin;
+
+            //// Получаем пользователя из базы данных по логину
+            //var user = _context.User.FirstOrDefault(u => u.Login == currentUserLogin);
+
+            //if (user != null)
+            //{
+            //    // Получаем userId из найденного пользователя
+            //    Guid userId = user.Id;
+
+            //    // Передаем userId в конструктор AccauntPage
+            //    ContentControlFrame.Content = new AccauntPage(userId);  // Вместо this.Content =
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Пользователь не найден.");
+            //}
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            ContentControlFrame.Content = new NumberPage();
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            ContentControlFrame.Content = new GuestPage();
+        }
+
+        
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            //ContentControlFrame.Content = new AdminServicePage(_currentUserId);
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            ContentControlFrame.Content = new ReservationPage();
+        }
+
+        private void Close_btn_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
         }
     }
 }
